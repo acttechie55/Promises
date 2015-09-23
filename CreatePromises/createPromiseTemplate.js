fa3361e51816a155977331d2bd1ab52da1a788ad
acttechie55 <actaylor55@gmail.com>
2015-09-23 15:43:15 -0500

    labs partially completed

--- CreatePromises/createPromiseTemplate.js
+++ CreatePromises/createPromiseTemplate.js
@@ -1,11 +1,22 @@
 var APromise = {};
 
-APromise.all = function(promises){};
+APromise.all = function(promises){
+	return Promise.all(promises);
+};
 
-APromise.race = function(promises){};
+APromise.race = function(promises){
+	return Promise.race(promises);
+	//Scratch work
+	//var promise = APromise.race();
+	//return promise;
+};
 
-APromise.resolve = function(value){};
+APromise.resolve = function(value){
+	return Promise.resolve(value);
+};
 
-APromise.reject = function(err){};
+APromise.reject = function(err){
+	return Promise.reject(err);
+};
 
 module.exports.APromise = APromise;
--- UsePromises/usePromiseTemplate.js
+++ UsePromises/usePromiseTemplate.js
@@ -1,5 +1,46 @@
 function mapAsync(iterator, obj, context) {
-	
+	var arr = [];
+	var n = obj.length;
+	for (var index = 0; index < n; index++) {
+		var collect = iterator.call(context, obj[index], index, obj);
+		arr.push(collect);
+	}
+	return Promise.all(arr);
 };
-function mapAsyncInOrder(iterator, array, context) {};
-function mapAsyncInDescendingOrder(iterator, array, context) {};
+function mapAsyncWithOrder(iterator, array, context, descending) {
+	//create a promise. We are saying that when it resolves, it resolves with an empty array
+	var initialValue = Promise.resolve([]);
+	if(!Array.isArray(array)) {
+		return initialValue;
+	}
+	iterator = iterator.bind(context);
+	var inOrder = function(prevValue, nextValue, nextIndex, array) {
+		return prevValue.then(function(items) {  
+			return iterator(nextValue, nextIndex, array).then(function(moreItems) {
+			return items.concat(moreItems);
+		});
+	});
+};
+function mapAsyncInOrder(iterator, array, context) {
+	var arr = [];
+	var n = array.length;
+	for (var index = 0; index < n; index++) {
+		var collect = iterator.call(context, array[index], index, array);
+		arr.push(collect);
+	}
+	return Promise.all(arr);
+};
+function mapAsyncInDescendingOrder(iterator, array, context) {
+	var arr = [].reduceRight;
+	var n = array.length;
+	for (var index = 0; index < n; index++) {
+		var collect = iterator.call(context, array[index], index, array);
+		arr.push(collect);
+	}
+	return Promise.all(arr);
+};
+
+//var flattened = [[0, 1], [2, 3], [4, 5]].reduceRight(function(a, b) {
+//    return a.concat(b);
+//}, []);
+// flattened is [4, 5, 2, 3, 0, 1]
